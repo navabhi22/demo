ef9257d6b969d11ebf13871bf51ac7cd631f7c4b
navabhi22 <navabhi22@gmail.com>
2022-02-17 09:23:27 +0530

    adding program and flipcoin

---
+++ FlipCoin.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipCoin
+{
+    public class FlipCoin
+    {
+        /// <summary>
+        /// Calculate Head Percentage and Tail Percentage
+        /// </summary>
+        /// <param name="flipCoin">The flip coin.</param>
+        public static void FlipTheCoin(int flipCoin)
+        {
+            //Console.WriteLine("Please enter the value for number of flips");
+
+            int Head = 0;
+            int Tail = 0;
+            int HeadCount = 0;
+            int TailCount = 0;
+
+            double HeadPercentage;
+            double TailPercentage;
+
+            //int flipCoin = Convert.ToInt32(Console.ReadLine());
+
+            if (flipCoin > 0)
+            {
+                for (int i = 0; i < flipCoin; i++)
+                {
+                    Random random = new Random();
+                    int number = random.Next(2);
+
+                    if (number == 1)
+                    {
+                        Head = 1;
+                        HeadCount += Head;
+                    }
+                    else if (number == 0)
+                    {
+                        Tail = 1;
+                        TailCount += Tail;
+
+                    }
+                    Console.WriteLine(number);
+
+                    
+                }
+
+                Console.WriteLine("----------------");
+
+                HeadPercentage = HeadCount * 100 / flipCoin;
+                TailPercentage = TailCount * 100 / flipCoin;
+
+                Console.WriteLine("Head Percentage " +HeadPercentage);
+                Console.WriteLine("Tail Percentage" +TailPercentage);
+
+            }
+            else
+            {
+                Console.WriteLine(" please enter a positive number ");
+            }
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FlipCoin
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Please enter the value for number of flips");
+            //int flipCoin = Convert.ToInt32(Console.ReadLine());
+            //FlipCoin.FlipTheCoin(flipCoin);
+
+            //SwitchDemo.DisplayMonth();
+
+            Console.WriteLine("enter the number");
+            int N = Convert.ToInt32(Console.ReadLine());
+            string num = EvenNumber.EvenNum(N);
+            Console.WriteLine(num);
+           
+        }
+    }
+}
